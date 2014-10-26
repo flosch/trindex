6464c8c8914077d87b354f0d642a23d490fc8e52
Florian Schlachter <flori@n-schlachter.de>
2014-10-26 14:37:09 +0100

    Create helper.go

---
+++ helper.go
@@ -0,0 +1,29 @@
+package trindex
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func abs_float(x float64) float64 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}

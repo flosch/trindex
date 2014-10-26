1fdeffd9c8ea5eca778eb4824e9f75da41e02fc2
Florian Schlachter <flori@n-schlachter.de>
2014-10-26 21:15:01 +0100

    only count any trigram once per input, no duplicates allowed.

--- trigram.go
+++ trigram.go
@@ -12,17 +12,21 @@ func trigramize(data string) (trigrams []string) {
 		data_runes = append(data_runes, ' ')
 	}
 
-	trigrams = make([]string, 0, len(data_runes)-2)
+	trigram_set := make(map[string]*struct{})
 
 	dl := len(data_runes)
 	for i := 0; i < dl-2; i++ {
-		trigrams = append(trigrams, string(data_runes[i:i+3]))
+		trigram_set[string(data_runes[i:i+3])] = nil
 	}
 
-	trigrams = append(trigrams, string(data_runes[dl-2:dl]))
-	trigrams = append(trigrams, string(data_runes[dl-1:dl]))
-	trigrams = append(trigrams, string(data_runes[0:1]))
-	trigrams = append(trigrams, string(data_runes[0:2]))
+	trigram_set[string(data_runes[dl-2:dl])] = nil
+	trigram_set[string(data_runes[dl-1:dl])] = nil
+	trigram_set[string(data_runes[0:1])] = nil
+	trigram_set[string(data_runes[0:2])] = nil
+
+	for k, _ := range trigram_set {
+		trigrams = append(trigrams, k)
+	}
 
 	return
 }
